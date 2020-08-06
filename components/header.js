6e1f9fd05d01abed08e533f180f9575c67b2dd6a
Shrey Parikh <parik081@umn.edu>
2020-08-06 14:42:18 -0500

    working header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,14 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    return <div>{/* your code here */}</div>;
+    return <div>{
+      <div className={styles.topnav}>
+      <a className={styles.active} href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+      }</div>;
   }
 }
 

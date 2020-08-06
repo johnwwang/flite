82edcdbb1e8d270a3b899d7f14b68cbea598ecf9
Jwing6872 <johnwang58@gmail.com>
2020-08-06 15:07:50 -0500

    working package.json

--- components/header.js
+++ components/header.js
@@ -4,14 +4,19 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    return <div>{
+    return (
       <div className={styles.topnav}>
-      <a className={styles.active} href="#home">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-    </div>
-      }</div>;
+        <a>
+          <img className={styles.headericon} src="birb.png"></img>
+        </a>
+        <a className={styles.active} href="#home">
+          Home
+        </a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    );
   }
 }
 
--- pages/app.js
+++ pages/app.js
@@ -1,70 +1,69 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Component } from 'react'
-import io from 'socket.io-client'
-import fetch from 'isomorphic-fetch'
+import { Component } from "react";
+import io from "socket.io-client";
+import fetch from "isomorphic-fetch";
 
 class AppPage extends Component {
   // fetch old messages data from the server
   static async getInitialProps({ req }) {
-    const response = await fetch('http://localhost:3000/messages')
-    const messages = await response.json()
-    return { messages }
+    const response = await fetch("http://localhost:3000/messages");
+    const messages = await response.json();
+    return { messages };
   }
 
   static defaultProps = {
     messages: [],
-  }
+  };
 
   // init state with the prefetched messages
   state = {
-    field: '',
+    field: "",
     messages: this.props.messages,
-  }
+  };
 
   // connect to WS server and listen event
   componentDidMount() {
-    this.socket = io('http://localhost:3000')
-    this.socket.on('message', this.handleMessage)
+    this.socket = io("http://localhost:3000");
+    this.socket.on("message", this.handleMessage);
   }
 
   // close socket connection
   componentWillUnmount() {
-    this.socket.off('message', this.handleMessage)
-    this.socket.close()
+    this.socket.off("message", this.handleMessage);
+    this.socket.close();
   }
 
   // add messages from server to the state
   handleMessage = (message) => {
-    this.setState(state => ({ messages: state.messages.concat(message) }))
-  }
+    this.setState((state) => ({ messages: state.messages.concat(message) }));
+  };
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({ field: event.target.value });
-  }
+  };
 
   // send messages to server and add them to the state
-  handleSubmit = event => {
-    event.preventDefault()
+  handleSubmit = (event) => {
+    event.preventDefault();
 
     // create message object
     const message = {
-      id: (new Date()).getTime(),
+      id: new Date().getTime(),
       value: this.state.field,
-    }
+    };
 
     // send object to WS server
-    this.socket.emit('message', message)
+    this.socket.emit("message", message);
 
     // add it to state and clean current input value
-    this.setState(state => ({
-      field: '',
-      messages: state.messages.concat(message)
-    }))
-  }
+    this.setState((state) => ({
+      field: "",
+      messages: state.messages.concat(message),
+    }));
+  };
 
-
-  render(){
+  render() {
     return (
       <div>
         <Head>
@@ -75,25 +74,25 @@ class AppPage extends Component {
             App
           </h1>
           <div>
-          <ul>
-            {this.state.messages.map(message =>
-              <li key={message.id}>{message.value}</li>
-            )}
-          </ul>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              onChange={this.handleChange}
-              type="text"
-              placeholder="Hello world!"
-              value={this.state.field}
-            />
-            <button>Send</button>
-          </form>
-        </div>
+            <ul>
+              {this.state.messages.map((message) => (
+                <li key={message.id}>{message.value}</li>
+              ))}
+            </ul>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                placeholder="Hello world!"
+                value={this.state.field}
+              />
+              <button>Send</button>
+            </form>
+          </div>
         </main>
       </div>
     );
   }
 }
 
-export default AppPage
+export default AppPage;

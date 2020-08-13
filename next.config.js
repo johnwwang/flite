8ac8cc69992ad6f7ca01cb9498fc517db25606a7
Jwing6872 <johnwang58@gmail.com>
2020-08-13 17:14:45 -0500

    hi

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     TEST_ENV: "200!",
-    BASE_URL: "https://flite-app.herokuapp.com",
+    BASE_URL: "http://flite-app.herokuapp.com",
     MONGO_URI:
       "mongodb+srv://flite:pokemen@flite.exc5x.mongodb.net/test?retryWrites=true&w=majority",
   },
